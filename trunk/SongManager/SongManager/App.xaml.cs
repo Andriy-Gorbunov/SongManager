51cf39f6d4c7a48dcff44cd7ee5f6f61fa6a701c
Androniq <Androniq@2a710487-0a78-439c-bec2-7d58a61f62e3>
2016-12-04 21:39:31 +0000

    SQLite

--- trunk/SongManager/SongManager/App.xaml.cs
+++ trunk/SongManager/SongManager/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,29 @@ namespace SongManager
     /// </summary>
     public partial class App : Application
     {
+        public SQLiteConnection Connection { get; private set; }
+
+        public const string SqlFileName = "songs.db";
+
+        public new static App Current { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Current = this;
+
+            base.OnStartup(e);
+
+            SQLiteConnection.CreateFile(SqlFileName);
+            Connection = new SQLiteConnection($"Data Source = {SqlFileName}");
+            Connection.Open();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Connection?.Close();
+            Connection = null;
+
+            base.OnExit(e);
+        }
     }
 }
