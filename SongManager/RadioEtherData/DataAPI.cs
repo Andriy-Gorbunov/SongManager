bc1970c5bc5a12f7c2af80d2dd4c9946c6cd2941
olga-jane <olga.jane.vakhromova@gmail.com>
2016-12-12 02:40:39 +0200

    RadioEtherData (db layer) and RadioEtherModel (model only. Automatically updated from Data appropriate edmx)

---
+++ SongManager/RadioEtherData/DataAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioEtherData
+{
+    public static class DataAPI
+    {
+        private static string connectionString = $"Data Source = songs.sdf";
+
+        static DataAPI()
+        {
+            var en = new SqlCeEngine(connectionString);
+            if (!en.Verify())
+            {
+                en.CreateDatabase();
+
+            }
+        }
+        public static void ReCreateDatabaseContent()
+        { 
+            using (var connection = new SqlCeConnection(connectionString))
+            {
+                connection.Open();
+
+                var script = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("RadioEtherData.Model.edmx.sql")).ReadToEnd();
+                foreach (var cmd in script.Split(new[] { "\r\nGO\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    using (var cmdd = new SqlCeCommand(cmd, connection))
+                    {
+                        try
+                        {
+                            cmdd.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Ignorable SQL exception: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
